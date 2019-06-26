1e28581c88b26c513f960be96be2ff7ab1b0d867
Jessica Standage <Jessica@Jessicas-MBP.socal.rr.com>
2019-06-26 13:11:20 -0700

    try 3

---
+++ Assignment 8, Pt 1, Try 3.js
@@ -0,0 +1,75 @@
+<p id="demo"></p>
+
+<script>
+
+// let ms1 = new MySet([1,2,3,4]);
+// ms1.add(4);
+// ms1.add(4);
+// ms1.add(5);
+// ms1.add(6);
+// ms1.remove(5);
+// console.log(ms1.toArray());
+// console.log(ms2.toArray());
+// console.log(ms2.has(3));
+// console.log(ms2.size());
+
+
+
+function myFunction(x.display) {
+console.log(toArray)
+};
+
+// Constructor function for Person objects
+class MySet {
+  constructor(add, remove, size, has){
+    this._add = add;
+    this._remove = remove;
+    this._size = size;
+    this._has = has;
+  }
+
+  describeMe(){
+    console.log('I' + this._add + 'and I' + this._remove + 'and I have' + this._size + 'and' + this._has);
+  }
+}
+
+let isEqual(other_set) = new MySet("add", "remove", "size", "has");
+isEqual(other_set).describeMe();
+
+let toArray() = new MySet("add", "remove", "size", "has");
+toArray().describeMe();
+
+let forEach(callback) = new MySet("add", "remove", "size", "has");
+forEach(callback).describeMe();
+
+let clone() = new MySet("add", "remove", "size", "has");
+clone().describeMe();
+
+
+//OR??BELOW??//
+    //
+    // myFunction(){
+    //   this._add = add;
+    //   isEqual(other_MySet) {
+    //   return this._add === other_MySet._add;
+    // }
+    //
+    // myFunction(){
+    //   this._remove = remove;
+    //   toArray(other_MySet) {
+    //   return this._remove === other_MySet._remove;
+    // }
+    //
+    // myFunction(){
+    //   this._size = size;
+    //   forEach(callback_MySet) {
+    //   return this._size === other_MySet._size;
+    // }
+    //
+    // myFunction(){
+    //   this._has = has;
+    //   clone(MySet) {
+    //   return this._has === other_MySet._has;
+    // }
+
+</script>
